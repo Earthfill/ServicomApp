0e62de7cd27ddb2e8ee4849e1f20bbebf4e75475
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-07-25 10:17:18 +0100

    complaint handling

--- src/modals/ModalComplaint.jsx
+++ src/modals/ModalComplaint.jsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 const states = [
   'Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa', 'Benue', 'Borno', 'Cross River', 'Delta', 'Ebonyi', 'Edo', 
   'Ekiti', 'Enugu', 'Gombe', 'Imo', 'Jigawa', 'Kaduna', 'Kano', 'Katsina', 'Kebbi', 'Kogi', 'Kwara', 'Lagos', 'Nasarawa', 
-  'Niger', 'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba', 'Yobe', 'Zamfara',
+  'Niger', 'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba', 'Yobe', 'Zamfara', 'FCT (Abuja)'
 ];
 
 // const lgasByState = {
