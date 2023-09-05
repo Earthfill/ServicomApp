16a088bcc3eb136f463422b848ce2f358666149e
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-09-05 12:12:16 +0100

    hosting

--- src/services/servicom.js
+++ src/services/servicom.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://b7cb7-197-210-76-136.ngrok-free.app'; // host.local.internal
+const BASE_URL = 'https://8def-197-210-53-128.ngrok-free.app'; // host.local.internal
 const ID_BASE_URL = `${BASE_URL}/api/v1/Agency/getAgencyByGuid?uniqueGuid=`;
 const COMPLAINT_URL = `${BASE_URL}/api/v1/Complaint`;
 
