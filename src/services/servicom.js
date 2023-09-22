d5d643ec01ab955a4903505ed8bb597bcef22124
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-09-22 14:16:26 +0100

    final commit

--- src/services/servicom.js
+++ src/services/servicom.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 // const BASE_URL = 'https://e249-197-210-70-37.ngrok-free.app'; // host.local.internal
 // const ID_BASE_URL = `${BASE_URL}/api/v1/Agency/getAgencyByGuid?uniqueGuid=`;
-const ID_BASE_URL = 'https://feeback-core.herokuapp.com/api/v1/agency/1'
+const ID_BASE_URL = 'https://feeback-core.herokuapp.com/api/v1/Agency/getAgencyByGuid?uniqueGuid=';
 const COMPLAINT_URL = 'https://feeback-core.herokuapp.com/api/v1/Complaint';
 const TAG_URL = 'https://feeback-core.herokuapp.com/api/v1/tag';
 
-const getByGuid = async () => {
-  const response = await axios.get(`${ID_BASE_URL}`);
+const getByGuid = async (uniqueGuid) => {
+  const response = await axios.get(`${ID_BASE_URL}${uniqueGuid}`);
   console.log(response);
   return response.data;
 }
